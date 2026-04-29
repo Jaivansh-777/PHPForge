a079adfc9e12833b2e1c03c6cbecaedae3e3a36d
JAIVANSH <jaivansh777@gmail.com>
2026-04-29 15:53:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -100,6 +100,7 @@ $m = $meta[$page];
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7390574521339742" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <title><?php echo $m['title']; ?></title>
     <meta name="description" content="<?php echo $m['desc']; ?>">
